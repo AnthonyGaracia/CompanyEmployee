e76db75fda9bb9cb8fb56a577f55672c2fa61bd8
Tony Garcia <tonygarcia@Tonys-Air.attlocal.net>
2024-01-02 18:06:12 -0800

    first commit

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const {
+  client,
+  seed
+} = require('./dbs')
+
 
 app.use('/dist', express.static(path.join(__dirname, '../dist')));
 app.use('/public', express.static(path.join(__dirname, '../public')));
@@ -8,8 +13,12 @@ app.use('/public', express.static(path.join(__dirname, '../public')));
 app.get('/', (req, res)=> res.sendFile(path.join(__dirname, '../public/index.html'))); 
 
 const init = async()=> {
+  await client.connect()
+  console.log("connected to database")
+  await seed()
 
-  console.log('do seed and sync here for database');
+  
+ 
 
   const port = process.env.PORT || 3000;
   app.listen(port, ()=> {
